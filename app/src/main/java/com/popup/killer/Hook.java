f6cf7fb18b4f2e65a0db6518b73f71233d28fd6d
michaeL <mkcs121@gmail.com>
2025-03-23 04:49:39 +0800

    added MacauLife

--- app/src/main/java/com/popup/killer/Hook.java
+++ app/src/main/java/com/popup/killer/Hook.java
@@ -1,8 +1,10 @@
 package com.popup.killer;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Hook implements IXposedHookLoadPackage {
@@ -39,6 +41,35 @@ public class Hook implements IXposedHookLoadPackage {
 
             } catch (Exception e) {
                 XposedBridge.log("Moneyback hook error: " + e);
+            }
+        }
+        // Hook for com.mem.MacaoLife
+        else if (lpparam.packageName.equals("com.mem.MacaoLife")) {
+            try {
+                Class<?> FullScreenADAlert = lpparam.classLoader.loadClass(
+                        "com.mem.life.ui.launch.fragment.LaunchAdsFragment"
+                );
+
+                // Hook setLaunchAd method
+                XposedBridge.hookAllMethods(FullScreenADAlert, "setLaunchAd", XC_MethodReplacement.DO_NOTHING);
+
+                // Hook checkNeedShow method
+                XposedHelpers.findAndHookMethod(
+                        "com.mem.life.ui.home.popup.HomeAdsFragment", // Full class name
+                        lpparam.classLoader,
+                        "checkNeedShow",                // Method name to hook
+                        // Add parameter types if the method takes any, e.g., String.class, int.class
+                        new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
+                                // Force the method to return false
+                                param.setResult(false);
+                            }
+                        }
+                );
+
+            } catch (Exception e) {
+                XposedBridge.log("MacaoLife hook error: " + e);
             }
         }
     }
